0370a2456425bf8267ec4f3766dcba928911a00a
daisymae <daisymae.orcadiafarm@gmail.com>
2019-02-13 13:18:14 -0600

    create store
    Create the redux store and basic reducer function. Still not being triggered by an action, so still not change to application behavior.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { createStore } = require('redux');
+
 const defaultState = {
   courses: [
     {
@@ -14,6 +16,13 @@ const defaultState = {
     }
   ]
 };
+
+function reducer(state, action) {
+  return state;
+}
+
+// create redux store
+const store = createStore(reducer, defaultState);
 
 function addView(viewFunc) {
   viewFunc(defaultState);
